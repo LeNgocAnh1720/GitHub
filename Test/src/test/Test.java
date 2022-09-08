6af6b2bf4c3b4f99159f95e0048a3793cbd97553
LeNgocAnh11 <87508681+LeNgocAnh11@users.noreply.github.com>
2022-09-08 14:20:27 +0700

    while

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -31,6 +31,12 @@ public class Test {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello2");
         }
+        
+        int i=0;
+        while(i<10){
+            System.out.println(i + "Hello");
+            i++;
+        }
     }
     
 }
