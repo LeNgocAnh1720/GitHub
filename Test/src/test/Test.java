dd66f6534071be36b055e63081504b0e03ce2f18
LeNgocAnh11 <87508681+LeNgocAnh11@users.noreply.github.com>
2022-09-08 14:00:22 +0700

    Hello1 LeNgocAnh
    main

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -16,6 +16,8 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("Hello1");
+        
+        System.out.println("Branch 1 LeNgocAnh");
     }
     
 }
